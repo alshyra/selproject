60403733fad7cac37ac3022d453444f78bccb064
antoine.savajols <antoine.savajols@gmail.com>
2018-02-26 22:23:27 +0100

    ajout description home page

---
+++ src/app/concept/concept.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConceptComponent } from './concept/concept.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ConceptComponent]
+})
+export class ConceptModule { }
