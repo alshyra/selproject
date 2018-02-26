1689349ca36d4f2f3ca5b713de7c06365b47c4bb
antoine.savajols <antoine.savajols@gmail.com>
2018-02-26 22:04:47 +0100

    ajout flex module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule, MatSidenavModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeModule } from './home/home.module';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -19,7 +19,8 @@ import { HomeModule } from './home/home.module';
     MatToolbarModule,
     MatIconModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
